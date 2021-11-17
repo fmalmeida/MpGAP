ee125c809396b593814b79cff5bb43ca85d8374c
Felipe Marques de Almeida <felipemarques89@gmail.com>
2021-11-17 12:07:01 -0300

    fixing conda prefix inside process

--- bin/get_quast_dbs.sh
+++ bin/get_quast_dbs.sh
@@ -1,10 +1,12 @@
-mkdir -p $CONDA_PREFIX/envs/mpgap-3.1/lib/python3.6/site-packages/quast_libs/busco/
+#!/bin/bash -ue
+
+mkdir -p $CONDA_PREFIX/lib/python3.6/site-packages/quast_libs/busco/
 ### bacteria db
-BACTERIA_DB="$CONDA_PREFIX/envs/mpgap-3.1/lib/python3.6/site-packages/quast_libs/busco/bacteria.tar.gz"
+BACTERIA_DB="$CONDA_PREFIX/lib/python3.6/site-packages/quast_libs/busco/bacteria.tar.gz"
 [ ! -f "$BACTERIA_DB" ] && wget -O "$BACTERIA_DB" https://busco.ezlab.org/v2/datasets/bacteria_odb9.tar.gz
 ### eukaryota db
-EUKARYOTA_DB="$CONDA_PREFIX/envs/mpgap-3.1/lib/python3.6/site-packages/quast_libs/busco/eukaryota.tar.gz"
+EUKARYOTA_DB="$CONDA_PREFIX/lib/python3.6/site-packages/quast_libs/busco/eukaryota.tar.gz"
 [ ! -f "$EUKARYOTA_DB" ] && wget -O "$EUKARYOTA_DB" https://busco.ezlab.org/v2/datasets/eukaryota_odb9.tar.gz
 ### fungi db
-FUNGI_DB="$CONDA_PREFIX/envs/mpgap-3.1/lib/python3.6/site-packages/quast_libs/busco/fungi.tar.gz"
+FUNGI_DB="$CONDA_PREFIX/lib/python3.6/site-packages/quast_libs/busco/fungi.tar.gz"
 [ ! -f "$FUNGI_DB" ] &&  wget -O "$FUNGI_DB" https://busco.ezlab.org/v2/datasets/fungi_odb9.tar.gz
